dc353e285df9b5cd54244f999a85207228ed7935
Jeff Hall <jeffahall94@gmail.com>
2018-01-11 13:50:54 -0500

    hope and a prayer

---
+++ FinalB.Tests/Controllers/MoviesControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// references
+using System.Linq;
+using FinalB.Controllers;
+using FinalB.Models;
+using System.Web.Mvc;
+using Moq;
+
+namespace FinalB.Tests.Controllers
+{
+    [TestClass]
+    public class MoviesControllerTest
+    {
+        MoviesController controller;
+        List<Movie> movies;
+        Mock<IMovieRepository> mock;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // arrange
+            mock = new Mock<IMovieRepository>();
+
+            movies = new List<Movie>
+            {
+                new Movie { MovieID = 1, Title = "Movie 1", Year = 2000, Revenue = 300000 },
+                new Movie { MovieID = 2, Title = "Movie 2", Year = 2005, Revenue = 250000 },
+                new Movie { MovieID = 3, Title = "Movie 2", Year = 2010, Revenue = 200000 }
+            };
+
+            mock.Setup(m => m.Movies).Returns(movies.AsQueryable());
+
+            controller = new MoviesController(mock.Object);
+        }
+
+        [TestMethod]
+        public void IndexValidLoadsMovies()
+        {
+
+            // Act
+        
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void DetailsValidId()
+        {
+            // act
+            string result = controller.Details();
+            // assert
+        }
+
+        [TestMethod]
+        public void DetailsInvalidId()
+        {
+            // act
+            string result = movies.FindIndex(5);
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DetailsMissingId()
+        {
+            // act
+            string result = null;
+            // assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ FinalB/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalB.Models;
+
+namespace FinalB.Controllers
+{
+    //[Authorize]
+    public class MoviesController : Controller
+    {
+        private IMovieRepository db;
+
+        // default constructor using Entity Framework Repo
+        public MoviesController()
+        {
+            this.db = new EFMoviesRepository();
+        }
+
+        // testing constructor using Mock Repo
+        public MoviesController(IMovieRepository mock)
+        {
+            this.db = mock;
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Studio);
+            return View(movies.ToList());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error");
+            }
+            Movie movie = db.Movies.SingleOrDefault(m => m.MovieID == id);
+            if (movie == null)
+            {
+                return View("Error");
+            }
+            return View(movie);
+        }
+
+        
+    }
+}
